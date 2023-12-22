6f397cec42831ffdf42498f5d2c3395f00aad252
Fabien KORGO <kograss20@gmail.com>
2023-12-22 10:36:34 +0100

    make it better

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -14,13 +14,22 @@ const dbPassword = process.env.DB_PASSWORD
 //     database: 'lendafriend',
 // });
 
+// const sequelize: Sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     host: 'ec2-34-242-154-118.eu-west-1.compute.amazonaws.com',
+//     port: 5432,
+//     username: 'qsupdhpaoxbudh',
+//     password: dbPassword,
+//     database: 'dcr1vprt2eetq0',
+// });
+
 const sequelize: Sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'ec2-34-242-154-118.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    username: 'qsupdhpaoxbudh',
+    dialect: 'mysql',
+    host: 'mysql-158687-0.cloudclusters.net',
+    port: 17312,
+    username: 'admin',
     password: dbPassword,
-    database: 'dcr1vprt2eetq0',
+    database: 'lendafriend',
 });
 
 export {sequelize};
